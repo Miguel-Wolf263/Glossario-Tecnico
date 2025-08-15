138a04c4637706362c2b81dcbdf5ff2526c3669f
MIGUEL PEDRINI WOLF <miguel_wolf@estudante.sesisenai.org.br>
2025-08-15 10:46:03 -0300

    subindo o projeto

---
+++ script.js
@@ -0,0 +1,8 @@
+const accordionHeaders = document.querySelectorAll('.accordion-header');
+
+accordionHeaders.forEach(header => {
+    header.addEventListener('click', () => {
+        const accordionItem = header.parentElement;
+        accordionItem.classList.toggle('active');
+    });
+});
